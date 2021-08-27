96ae5cfc803ee7dd0f0da29cb353a71f52f259b6
VoidSpaceXYZ <null@voidspace.xyz>
2021-08-27 20:00:39 +0530

    New list. Currently no change

--- generatelist.sh
+++ generatelist.sh
@@ -1,3 +1,4 @@
 git clone https://github.com/AmnestyTech/investigations/
 cd investigations
 cat */*domains.txt | sort | uniq | tee -a amensty_nso_domain_list.txt
+cp amensty_nso_domain_list.txt ../amensty_nso_domain_list.txt
