49d5967cba71d02772baa634742fd0c37927ea66
voidspacexyz <null@voidspace.xyz>
2022-01-16 02:42:49 +0000

    Fixed a bug in the code generation script

--- generatelist.sh
+++ generatelist.sh
@@ -2,3 +2,4 @@ git clone https://github.com/AmnestyTech/investigations/
 cd investigations
 cat */*domains.txt | sort | uniq | tee -a amensty_nso_domain_list.txt
 cp amensty_nso_domain_list.txt ../amensty_nso_domain_list.txt
+rm amensty_nso_domain_list.txt
